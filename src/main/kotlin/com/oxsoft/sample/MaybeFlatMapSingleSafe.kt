998a172a8f026d275c9add3317b391ef5de5fa34
oxsoft <oxsoft7@gmail.com>
2017-10-01 21:29:25 +0900

    update comment

--- src/main/kotlin/com/oxsoft/sample/MaybeFlatMapSingleSafe.kt
+++ src/main/kotlin/com/oxsoft/sample/MaybeFlatMapSingleSafe.kt
@@ -7,6 +7,6 @@ import io.reactivex.Single
 fun <T, R> Maybe<T>.flatMapSingleSafe(mapper: (T?) -> Single<R>): Single<R> =
         this.flatMapSingleElement(mapper).switchIfEmpty(Single.just(Unit).flatMap { mapper(null) })
 
-/** 以下のようにしてしまうと、値がある時に[mapper]が2回評価されてしまう */
+/** 以下のようにしてしまうと、mapper(null)が呼び出し時に評価されてしまう */
 fun <T, R> Maybe<T>.flatMapSingleSafe2(mapper: (T?) -> Single<R>): Single<R> =
         this.flatMapSingleElement(mapper).switchIfEmpty(mapper(null))
--- src/main/kotlin/com/oxsoft/sample/MaybeMapToSingle.kt
+++ src/main/kotlin/com/oxsoft/sample/MaybeMapToSingle.kt
@@ -6,5 +6,5 @@ import io.reactivex.Single
 /** emptyの時は[mapper]がnullで呼び出される。[mapper]が[R]型のNonNull値を返すことで[Single]に変換する。 */
 fun <T, R> Maybe<T>.mapToSingle(mapper: (T?) -> R) = this.map(mapper).switchIfEmpty(Single.fromCallable { mapper(null) })
 
-/** 以下のようにしてしまうと、値がある時に[mapper]が2回評価されてしまう */
+/** 以下のようにしてしまうと、mapper(null)が呼び出し時に評価されてしまう */
 fun <T, R> Maybe<T>.mapToSingle2(mapper: (T?) -> R) = this.map(mapper).toSingle(mapper(null))
